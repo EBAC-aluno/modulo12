b7fcae6ba5b29c38980754815ba4abf7ef7b1487
Pedro Lobo <lobophf@gmail.com>
2022-10-13 17:42:24 -0300

    Update AppA.java

--- AppA.java
+++ AppA.java
@@ -1,15 +1,23 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class AppA {
   public static void main(String args[]) {
 
+    String input = "";
     for (int i = 0; i < args.length; i++) {
-      args[i] = args[i].replace(",", "");
+      input += " " + args[i];
+    }
+    
+    ArrayList<String> list = new ArrayList<String>();
+    String parts[] = input.split(",");
+      for(int i = 0; i < parts.length; i++){
+      list.add(parts[i].replaceFirst("^\\s+", ""));
     }
 
-    Arrays.sort(args);
+    Collections.sort(list);   
 
-    for (String s: args){
+    for (String s: list){
       System.out.println(s);
     }
 
